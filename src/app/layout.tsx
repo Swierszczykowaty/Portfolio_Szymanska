3184ac51fcacd64c4f16d0a0ca27d06cfec43a68
Michał Świercz <loolok1232@gmail.com>
2024-12-14 19:14:59 +0100

    Revert "[FIX] - html file"
    This reverts commit f3b529db09c1a85997fe6748f8c57a2f648c01ac.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import "./globals.css";
-import { Host_Grotesk } from 'next/font/google';
+
+import {Host_Grotesk} from 'next/font/google'
 
 const hostGrotesk = Host_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800'],
-  style: ['normal', 'italic'],
+  subsets: ['latin'], 
+  weight: ['300', '400', '500', '600', '700', '800'], 
+  style: ['normal', 'italic'], 
   display: 'swap',
-  variable: '--font-host-grotesk',
+  variable: '--font-host-grotesk', 
 });
 
 export default function RootLayout({
@@ -16,13 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${hostGrotesk.variable}`}>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <meta name="description" content="Your description here" />
-        <title>Your Page Title</title>
-      </head>
       <body className="font-sans">
         <main>{children}</main>
       </body>
