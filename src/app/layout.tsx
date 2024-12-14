f3b529db09c1a85997fe6748f8c57a2f648c01ac
Michał Świercz <loolok1232@gmail.com>
2024-12-14 18:45:17 +0100

    [FIX] - html file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import "./globals.css";
-
-import {Host_Grotesk} from 'next/font/google'
+import { Host_Grotesk } from 'next/font/google';
 
 const hostGrotesk = Host_Grotesk({
-  subsets: ['latin'], 
-  weight: ['300', '400', '500', '600', '700', '800'], 
-  style: ['normal', 'italic'], 
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800'],
+  style: ['normal', 'italic'],
   display: 'swap',
-  variable: '--font-host-grotesk', 
+  variable: '--font-host-grotesk',
 });
 
 export default function RootLayout({
@@ -17,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${hostGrotesk.variable}`}>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="description" content="Your description here" />
+        <title>Your Page Title</title>
+      </head>
       <body className="font-sans">
         <main>{children}</main>
       </body>
