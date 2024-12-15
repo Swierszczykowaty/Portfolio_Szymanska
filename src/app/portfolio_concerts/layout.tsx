c14e826f0d7e8ac99eced6e57aca020bbee5d981
Michał Świercz <loolok1232@gmail.com>
2024-12-15 13:38:09 +0100

    [FIX] - Wraper letter

--- src/app/portfolio_concerts/layout.tsx
+++ src/app/portfolio_concerts/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/Components/footer_comp";
-import Wrapper from "@/Components/Wrapper";
+import Wrapper from "@/Components/wrapper_comp";
 import Nav from "@/Components/nav_comp";
 
 export default function RootLayout({
